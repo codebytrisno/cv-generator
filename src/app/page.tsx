5e299b2d07b6667fc9446393e75eb452ba36b72d
TrisnoSanjaya <email.trisno.sanjaya@gmail.com>
2026-07-03 21:26:20 +0700

    remove stats bar from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { HeroSection } from '@/components/home/HeroSection'
-import { StatsBar } from '@/components/home/StatsBar'
 import { FeatureCards } from '@/components/home/FeatureCards'
 import { HowItWorks } from '@/components/home/HowItWorks'
 import { TemplateCarousel } from '@/components/home/TemplateCarousel'
@@ -12,9 +11,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      <RevealSection>
-        <StatsBar />
-      </RevealSection>
       <RevealSection>
         <FeatureCards />
       </RevealSection>
